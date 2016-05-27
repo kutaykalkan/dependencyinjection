b72c5adf065a3ee38637d91415717ef2c36bb019
TechLead <TechLead@skystem.com>
2016-05-27 13:10:06 +0000

    Local Environment setup

---
+++ ARTService/ARTService/Log/EventLogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SkyStem.ART.Service.Utility;
+using System.Diagnostics;
+
+namespace SkyStem.ART.Service.Log
+{
+    public class EventLogger
+    {
+        public const string EVENT_SOURCE_NAME = "SkyStemARTServiceLog";
+        public const string EVENT_LOG_NAME = "";
+
+        public static void CreateEventLog()
+        {
+            if (!EventLog.SourceExists(EventLogger.EVENT_SOURCE_NAME))
+            {
+                EventLog.CreateEventSource(EventLogger.EVENT_SOURCE_NAME, EventLogger.EVENT_LOG_NAME); //"Application");
+                System.Threading.Thread.Sleep(2000); 
+            }
+        }
+
+        internal static void WriteLogEntry(string message)
+        {
+            EventLog oEventLog = new EventLog(EventLogger.EVENT_LOG_NAME);
+            oEventLog.Source = EventLogger.EVENT_SOURCE_NAME;
+            oEventLog.WriteEntry(message);
+        }
+    }
+}
