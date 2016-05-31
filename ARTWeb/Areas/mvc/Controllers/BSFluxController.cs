2ca86bb7c9918cb0ffed635a58080a88b873db92
cliff <cgray@skystem.com>
2016-05-31 17:11:59 +0000

    feat: Add mvc support under a new area for mvc.

---
+++ ARTWeb/Areas/mvc/Controllers/BSFluxController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SkyStem.ART.Web.Areas.mvc.Controllers
+{
+    public class BSFluxController : Controller
+    {
+        // GET: mvc/BSFlux
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ARTWeb/Areas/mvc/mvcAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SkyStem.ART.Web.Areas.mvc
+{
+    public class mvcAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "mvc";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "mvc_default",
+                "{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
