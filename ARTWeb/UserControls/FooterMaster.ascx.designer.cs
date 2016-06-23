fd5619023b7547e7f0b6054e8d7189571b84393a
cliff <cgray@skystem.com>
2016-06-23 16:30:21 +0000

    feat: ui updates, system pages, account pages, tasks, etc., Still WIP

--- ARTWeb/UserControls/FooterMaster.ascx.designer.cs
+++ ARTWeb/UserControls/FooterMaster.ascx.designer.cs
@@ -12,6 +12,15 @@
 public partial class UserControls_FooterMaster {
     
     /// <summary>
+    /// Image1 control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.Image Image1;
+    
+    /// <summary>
     /// lblFooterCopyright control.
     /// </summary>
     /// <remarks>
